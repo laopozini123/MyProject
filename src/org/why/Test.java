6d4b654d47aa4955bbb7adda0f2095bbe17a32ce
laopozini123 <1048097081@qq.com>
2020-05-05 14:55:30 +0800

    0.8
    简单测试类

---
+++ src/org/why/Test.java
@@ -0,0 +1,13 @@
+package org.why;
+
+/**
+ * @author ：why
+ * @date ：Created in 2020/5/5 14:54
+ * @description：
+ * @version:
+ */
+public class Test {
+    public static void main(String[] args){
+
+    }
+}
