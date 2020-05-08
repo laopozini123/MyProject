9f706c5dc803b10dda6800558c197a3f1856984a
laopozini123 <1048097081@qq.com>
2020-05-08 08:57:50 +0800

    0.9
    基础

---
+++ NIO/src/org/why/CSChannel/Server.java
@@ -0,0 +1,29 @@
+package org.why.CSChannel;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+
+/**
+ * @author ：why
+ * @date ：Created in 2020/5/5 19:35
+ * @description：
+ * @version:
+ */
+public class Server {
+    public static void main(String[] args) throws IOException {
+        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open().bind(new InetSocketAddress(9999));
+        SocketChannel socketChannel = serverSocketChannel.accept();
+        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+        socketChannel.read(byteBuffer);
+        byteBuffer.flip();
+        System.out.println(new String(byteBuffer.array(), 0, byteBuffer.limit()));
+
+        ByteBuffer byteBufferOut = ByteBuffer.allocate(1024);
+        byteBufferOut.put("我是服务器,我来服务你了~".getBytes());
+        byteBufferOut.flip();
+        socketChannel.write(byteBufferOut);
+    }
+}
